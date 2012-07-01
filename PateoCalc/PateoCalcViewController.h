6e6a8acde64788b531b23e9f8f6ab222be58a06a
Charles Belcher <cfbelcher@Charles-Belchers-MacBook-Pro.local>
2012-06-30 21:35:35 -0400

    Initial Commit

---
+++ PateoCalc/PateoCalcViewController.h
@@ -0,0 +1,13 @@
+//
+//  PateoCalcViewController.h
+//  PateoCalc
+//
+//  Created by Charles Belcher on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PateoCalcViewController : UIViewController
+
+@end
