36cea45ab9c6c84cd06c8bfc4065c743adf8cccf
Chuck Belcher <cfbelcher@gmail.com>
2012-07-10 10:38:43 -0400

    add company

--- PateoCalc/PateoCalcModel.m
+++ PateoCalc/PateoCalcModel.m
@@ -3,7 +3,7 @@
 //  PateoCalc
 //
 //  Created by Charles Belcher on 6/30/12.
-//  Copyright (c) 2012 PateoGroup Inc. All rights reserved.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import "PateoCalcModel.h"
